137b4e7cca317d46545429e16ed48fbd956765bc
Andrey Melkov <andrey.melkov@gmail.com>
2021-01-08 12:54:48 +0300

    Task 5

--- lesson3/task_5.py
+++ lesson3/task_5.py
@@ -1,3 +1,26 @@
 """
 5. Программа запрашивает у пользователя строку чисел, разделенных пробелом. При нажатии Enter должна выводиться сумма чисел. Пользователь может продолжить ввод чисел, разделенных пробелом и снова нажать Enter. Сумма вновь введенных чисел будет добавляться к уже подсчитанной сумме. Но если вместо числа вводится специальный символ, выполнение программы завершается. Если специальный символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной ранее сумме и после этого завершить программу.
 """
+
+summa = 0
+exit_symbol = False
+
+while True:
+    number_str = input('Enter numbers separated by space (special symbol # to stop): ')
+    number_arr = number_str.split()
+    for el in number_arr:
+        try:
+            number = float(el)
+            summa += number
+        except ValueError as ex:
+            print(ex)
+            print('It\'s not a number')
+            if el == '#': 
+                exit_symbol = True
+                break
+
+    if exit_symbol:
+        break
+
+print('Summa:', summa)
+        
